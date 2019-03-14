c331375d99157d02e28274a57d025ed49e4584e5
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-14 15:35:47 +0000

    build based on 422e7f2

---
+++ dev/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "FriendlyDoLoop.jl",
+    "title": "FriendlyDoLoop.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#FriendlyDoLoop.CellPointToSValue",
+    "page": "FriendlyDoLoop.jl",
+    "title": "FriendlyDoLoop.CellPointToSValue",
+    "category": "type",
+    "text": "Abstract type representing a collection of values defined on a set of local points in each of the cells of the mesh. if cell_point_to_value is an instance of this type value = cell_point_to_value[cell][point] will be the value associated with the cell id cell, and local point id point\n\n\n\n\n\n"
+},
+
+{
+    "location": "#FriendlyDoLoop.jl-1",
+    "page": "FriendlyDoLoop.jl",
+    "title": "FriendlyDoLoop.jl",
+    "category": "section",
+    "text": "Dummy documentation pages created for testing purposesModules = [FriendlyDoLoop,]\nOrder   = [:function, :type]"
+},
+
+]}
