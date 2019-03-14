6e1da93cc72bb780b8b839daa8a5cec188f3dc6b
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-03-14 10:19:08 +0100

    Update cell_point_to_s_value.jl

--- src/cell_point_to_s_value.jl
+++ src/cell_point_to_s_value.jl
@@ -2,6 +2,10 @@
 """
 Abstract type representing a collection of values defined
 on a set of local points in each of the cells of the mesh.
+if `cell_point_to_value` is an instance of this type
+`value = cell_point_to_value[cell][point]`
+`value` will be the value associated with the cell id `cell`,
+and local point id `point`
 """
 
 abstract type CellPointToSValue{T} end
