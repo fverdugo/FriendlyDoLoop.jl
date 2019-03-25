e4e0f8dfd3b998863b9fada04fc903dea13a3493
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-25 18:35:42 +0000

    build based on 8514867

--- dev/search_index.js
+++ dev/search_index.js
@@ -9,14 +9,6 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#FriendlyDoLoop.CellPointToSValue",
-    "page": "FriendlyDoLoop.jl",
-    "title": "FriendlyDoLoop.CellPointToSValue",
-    "category": "type",
-    "text": "Abstract type representing a collection of values defined on a set of local points in each of the cells of the mesh. if cell_point_to_value is an instance of this type value = cell_point_to_value[cell][point] will be the value associated with the cell id cell, and local point id point\n\n\n\n\n\n"
-},
-
-{
     "location": "#FriendlyDoLoop.jl-1",
     "page": "FriendlyDoLoop.jl",
     "title": "FriendlyDoLoop.jl",
